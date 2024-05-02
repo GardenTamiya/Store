6d8434782477764ea46eca987ef62d27e5452ff5
GardenTamiya <168737029+GardenTamiya@users.noreply.github.com>
2024-05-02 18:48:00 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var reasonCarousel = new Flickity('#reasonCarousel', {
+    // Options
+    cellAlign: 'left',
+    contain: true,
+    wrapAround: true,
+    autoPlay: 5000 // Change slide every 5 seconds
+  });
